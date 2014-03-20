244e6aabffd8835949518f432c798d414bba06de
stebui <stefan_bui@hotmail.com>
2014-03-20 15:52:31 +0100

    flyttet collision

--- Project/src/com/example/curling/CurlingStone.java
+++ Project/src/com/example/curling/CurlingStone.java
@@ -12,7 +12,7 @@ public class CurlingStone extends Sprite{
 	
 	private static final String TAG = MainActivity.class.getSimpleName();
 	
-	private CurlingStone collidedStone = null; 
+	private Sprite collidedStone = null; 
 	private boolean moved = false;
 	private float speedX;
 	private float speedY;
@@ -27,6 +27,8 @@ public class CurlingStone extends Sprite{
 	private static Image red = new Image(R.drawable.curling);
 	private static Image yellow = new Image(R.drawable.curlingyellow);
 	private int stoneIndex;
+	
+	private double picLength; 
 	
 	
 	
@@ -44,6 +46,7 @@ public class CurlingStone extends Sprite{
         this.SPIN = 0;
         this.diff = diff();
         this.ay = this.ax*this.diff;
+        this.picLength = red.getHeight();
 
 	}
 	
@@ -104,7 +107,13 @@ public class CurlingStone extends Sprite{
     public float getSpeedY(){
 		return this.speedY;
 	}
-    public void setSpeedY(float speed) { this.speedY = speed; }
+    
+    public void setAy(float ay){
+    	this.ay = ay;
+    }
+    
+    public void setSpeedY(float speed){ 
+    	this.speedY = speed; }
 
     //find speed in y-direction to get the scaling correct with the x-speed
     public float diff(){
@@ -115,11 +124,11 @@ public class CurlingStone extends Sprite{
         return this.moved;
     }
 
-    public void setCollidedStone(CurlingStone stone){
+    public void setCollidedStone(Sprite stone){
 		this.collidedStone = stone; 
 	}
 	
-	public CurlingStone getCollidedStone(){
+	public Sprite getCollidedStone(){
 		return this.collidedStone;
 	}
     //perfect velocity
@@ -135,6 +144,61 @@ public class CurlingStone extends Sprite{
 		this.stoneIndex = stoneIndex;
 		return stoneIndex;
 	}
+	
+	public double getLengthOfStone(){
+		return picLength; 
+	}
 
+	public double getLengthBetweenStone(double dx, double dy){
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+	
+	public double getDx(Sprite stoneHitter, Sprite stoneHurt){
+		return Math.abs(stoneHurt.getPosition().getX()-stoneHitter.getPosition().getX());
+	}
+	
+	public double getDy(Sprite stoneHitter, Sprite stoneHurt){
+		return Math.abs(stoneHurt.getPosition().getY()-stoneHitter.getPosition().getY());
+	}
+	
+	public void collision(Sprite sprite){
+		double dx = getDx(this, sprite);
+		double dy = getDy(this, sprite);
+		
+		if (getLengthOfStone() >= getLengthBetweenStone(dx,dy)){
 
+			this.setCollidedStone(sprite);
+			((CurlingStone)sprite).setCollidedStone(this);
+			
+			double ax=dx/getLengthOfStone(), ay=dy/getLengthOfStone();
+			
+			//component of velocity in the direction of (dx,dy). Projection of the velocities in these axes
+			double va1=(this.getSpeedX()*ax+this.getSpeedY()*ay); 
+			double vb1=(-this.getSpeedX()*ay+this.getSpeedY()*ax); 
+			
+			double va2=(((CurlingStone)sprite).getSpeedX()*ax+((CurlingStone)sprite).getSpeedY()*ay);
+			double vb2=(-((CurlingStone)sprite).getSpeedX()*ay+((CurlingStone)sprite).getSpeedY()*ax);
+			
+			double ed = 1; //elastic collision
+			double mass = 20;
+			
+			// New velocities in these axes (after collision)
+			double vaP1=va1 + (1+ed)*(va2-va1)/(1+mass/mass);
+			double vaP2=va2 + (1+ed)*(va1-va2)/(1+mass/mass);
+			
+			double vx1=vaP1*ax-vb1*ay,  vy1=vaP1*ay+vb1*ax;// new vx,vy for ball 1 after collision
+			double vx2=vaP2*ax-vb2*ay,  vy2=vaP2*ay+vb2*ax;// new vx,vy for ball 2 after collision
+			
+			this.setSpeedX((float)vx1);
+			this.setSpeedY((float)vy1);
+			
+			this.setAy(((float)ay));
+			
+			((CurlingStone)sprite).setSpeedX((float)vx2);
+			((CurlingStone)sprite).setSpeedY((float)vy2);
+			
+			((CurlingStone)sprite).setAy(((float)ay));
+		}
+		
+	}
 }
